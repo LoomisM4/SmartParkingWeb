0c08f54e27416872701fd5a830d68ebc6301afd8
LoomisM4 <github@marcel-wettach.com>
2019-06-29 11:01:16 +0200

    more detailed message while loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,24 @@
 import React, {Component} from "react";
 import "../style/App.css"
+import {Colors} from "../settings/Colors";
 
 export default class Loading extends Component {
     render() {
         return (
-            <div className={"loadingScreen"}>
-                Laden... <br/>Bitte warten Sie einen Moment
+            <div className={"loadingScreen"} style={{backgroundColor: Colors.loadingScreenBackground}}>
+                Laden...
+                <br/>
+                Bitte warten Sie einen Moment
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                Falls Sie diese Seite dauerhaft sehen:
+                <br/>
+                Deaktivieren Sie die ursprungsübergreifende Beschränkung Ihres Browsers.
+                <br/>
+                Anschließend muss die Seite aktualisiert werden.
             </div>
         )
     }
